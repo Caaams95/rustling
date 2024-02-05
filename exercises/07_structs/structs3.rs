224219ef0e542af00ab24a5650142a76c572e3ed
Caaams95 <camillelctmlc@gmail.com>
2024-02-05 23:09:59 +0100

    WU 07_structs OK

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -1,3 +1,43 @@
+/// Write up:
+/// Ici on souhaite calculer les frais de port d'un envoi de coli.
+/// Dans notre cas, le package (coli) est composé de 3 criteres : pays d'expedition/ reception et le poids,
+/// On fait donc une structure qui ferra office d'objet contenant ses 3 variables.
+/// Notre structure sera definit comme ceci:
+///     - sender_country: String, ==> sender_country sera donc une chaine de caractère representant le pays d'expedition
+///     - recipient_country: String, ==> recipient_country sera donc une chaine de caractère representant le pays de reception
+///     - weight_in_grams: u32, ==> ici u32 représente un entier non signé de 32 bits qui sera le poids du package
+
+/// fn is_international(&self) -> bool {
+///     self.sender_country != self.recipient_country
+/// }
+/// 
+/// Pour commencer, -> bool signifie que la fonction retournera un booléen car ici on veux savoir si Oui ou Non
+/// l'expedition se fera a l'etranger
+/// Ici, &self indique que la méthode is_international prend une référence (&) à self,
+/// qui est une convention en Rust pour représenter l'instance sur laquelle la méthode est appelée,
+/// la fonction etant appeler comme ceci par exemple:
+///     package.is_international()
+/// cela signifie donc que self correspond a "package"
+/// 
+/// On compare donc si le sender_country et le recipient_country du package sont differents
+/// et on retournera donc sous forme de booléen (True ou False) si les pays sont différents 
+/// PS : pas de ; pour retourner une variable
+/// 
+/// 
+/// 
+/// Suite à ça nous avons la fonction:
+/// 
+/// fn get_fees(&self, cents_per_gram: u32) -> u32 {
+///     self.weight_in_grams * cents_per_gram
+/// }
+/// 
+/// celle ci retournera un nombre u32 car ici on souhaite savoir le prix des frais
+/// on a donc comme parametre encore une fois self ce qui nous permettra de recuperer le poids du package 
+/// et cents_per_gram: u32 qui est un nombre.
+/// Multiplier ces 2 nombres nous donnera donc le prix des frais s'il doit en avoir ( depend de is_international())
+/// 
+/// 
+
 // structs3.rs
 //
 // Structs contain data, but can also have logic. In this exercise we have
