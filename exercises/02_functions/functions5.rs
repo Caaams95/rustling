0565f33760f807a7d85fd62f7d0b3ac389f184dc
Caaams95 <92918705+Caaams95@users.noreply.github.com>
2024-02-07 18:21:03 +0100

    WU functions5.rs OK

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,3 +1,8 @@
+/// Write up
+/// Ici, nous souhaitons retourner le resultat de la fonction dans une variable.
+/// Nous devons donc retourner le resultat dans la fonctions square()
+/// Pour cela il ne faut pas mettre de ; a la fin de num*num afin de retourner cette variable
+
 // functions5.rs
 //
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
