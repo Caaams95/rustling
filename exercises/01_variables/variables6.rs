c77a6763ba4fa566a3ed5726692665425b02a90b
Caaams95 <camillelctmlc@gmail.com>
2024-02-07 18:12:32 +0100

    WU 01_variables OK

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,3 +1,7 @@
+/// Wirte up
+/// const NUMBER : i32 = 3;
+/// metre un ": <type>" après le nom d'une variable sert a definir le type de cette variable
+
 // variables6.rs
 //
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
