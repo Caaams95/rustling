fdbc086646ba98dca56b20fea380c55d115d1462
Caaams95 <camillelctmlc@gmail.com>
2024-02-07 16:04:01 +0100

    WU 14_generics OK

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -1,3 +1,40 @@
+/// Write up 
+/// struct Wrapper<T> {
+///     value: T,
+/// }
+/// La première ligne déclare une structure appelée Wrapper.
+/// La lettre T entre les chevrons < > indique que la structure est générique et 
+/// peut être paramétrée par un type. 
+/// Cela signifie que vous pouvez créer une instance de Wrapper en spécifiant
+/// n'importe quel type à la place de T.
+/// La structure a un seul champ appelé value qui est de type T.
+/// 
+/// impl<T> Wrapper<T> {
+///     pub fn new(value: T) -> Self {
+///         Wrapper { value }
+///     }
+/// }
+/// 
+/// Les lignes suivantes déclarent une implémentation pour la structure Wrapper 
+/// avec le paramètre de type générique T. 
+/// Cela signifie qu'on fournit une implémentation spécifique pour 
+/// cette structure lorsqu'elle est utilisée avec un type particulier T.
+/// 
+/// new est une méthode associée qui est utilisée pour créer une nouvelle instance 
+/// de la structure Wrapper.
+/// Cette méthode prend un argument value de type T et renvoie une nouvelle 
+/// instance de Wrapper avec ce value. 
+/// La méthode utilise la syntaxe de la structuration pour initialiser la 
+/// structure avec le champ value.
+/// 
+/// pub signifie que la méthode new est publique et peut être utilisée
+/// en dehors du module actuel.
+/// 
+/// self est utilisé pour indiquer le type actuel, c'est-à-dire 
+/// le type Wrapper<T> dans ce contexte.
+/// 
+/// 
+
 // generics2.rs
 //
 // This powerful wrapper provides the ability to store a positive integer value.
@@ -8,12 +45,12 @@
 
 // I AM NOT DONE
 
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
+impl<T> Wrapper<T> {
+    pub fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
